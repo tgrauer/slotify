56519f940cd0a9394bda4027a0ddb043997ebc6d
Thomas Grauer <tgrauer1@me.com>
2019-02-08 08:24:03 -0500

    Playbar built

--- index.php
+++ index.php
@@ -40,18 +40,97 @@
 
 <body>
 
-    <div class="container-fluid" id="nowPlayingBarCnt">
-        <div id="nowPlayingBar">
-            <div class="col-sm-4" id="nowPlayingLeft"></div>
-            <div class="col-sm-4" id="nowPlayingCenter">
-                <div class="content playerControls">
-                    <div class="buttons">
-                        <button class="btn"><i class="far fa-play-circle"></i></button>
-                        <button class="btn"><i class="fas fa-random"></i></button>
+    <div class="container-fluid" id="main_cnt">
+        <div class="row top_cnt">
+            <div class="col-sm-3 nav_sidebar">
+                <nav class="nav_bar">
+                    <a href="#" class="logo"><img src="img/logo.png" alt=""></a>
+                </nav>
+
+                <div class="group">
+                    <div class="nav_item">
+                        <a href="search.php" class="nav_link">Search <span><i class="fas fa-search"></i></span></a>
+                    </div>    
+                </div>
+
+                <div class="group">
+                    <div class="nav_item">
+                        <a href="browse.php" class="nav_link">Browse</a>
+                    </div>
+
+                    <div class="nav_item">
+                        <a href="yourmusic.php" class="nav_link">Your Music</a>
+                    </div>
+
+                    <div class="nav_item">
+                        <a href="profile.php" class="nav_link">Profile</a>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-4" id="nowPlayingRight"></div>
+
+            <div class="col-sm-9">
+                
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid" id="nowPlayingBarCnt">
+        <div id="nowPlayingBar">
+            <div class="col-sm-4" id="nowPlayingLeft">
+                <div class="content">
+                    <span class="album_link">
+                        <img src="img/album_cover.jpg" alt="" class="img-responsive album_artwork">
+                    </span>
+
+                    <div class="track_info">
+                        <span class="track_name">
+                            <span>Denial</span>
+                        </span>
+
+                        <span class="artist_name">
+                            <span>Sevendust</span>
+                        </span>
+                    </div>
+                </div>    
+            </div>
+
+            <div class="col-sm-4" id="nowPlayingCenter">
+
+                <div class="content playerControls">
+                    <div class="buttons">
+                        <button class="btn controlBtn shuffle" title="Shuffle"><i class="fas fa-random"></i></button>
+                        <button class="btn controlBtn previous" title="Previous"><i class="fas fa-step-backward"></i></button>
+                        <button class="btn controlBtn play" title="Play"><i class="fas fa-play-circle"></i></button>
+                        <button class="btn controlBtn pause" title="Pause" style="display: none;"><i class="fas fa-pause-circle"></i></button>
+                        <button class="btn controlBtn next" title="Next"><i class="fas fa-step-forward"></i></button>
+                        <button class="btn controlBtn repeat" title="Repeat"><i class="fas fa-redo-alt"></i></button>
+                    </div>
+                </div>
+
+                <div class="playback_bar">
+                    <span class="progress_time current">0.00</span>
+                    <div class="progressbar">
+                        <div class="progressbar_bg">
+                            <div class="progress_cur"></div>
+                        </div>
+                    </div>
+                    <span class="progress_time remaining">0.00</span>
+                </div>
+
+            </div>
+
+            <div class="col-sm-4" id="nowPlayingRight">
+                <div class="volume_bar">
+                    <button class="btn controlBtn volume" title="Volume"><i class="fas fa-volume-up"></i></button>
+
+                    <div class="progressbar">
+                        <div class="progressbar_bg">
+                            <div class="progress_cur"></div>
+                        </div>
+                    </div>
+                </div>    
+            </div>
+
         </div>
     </div>
 
